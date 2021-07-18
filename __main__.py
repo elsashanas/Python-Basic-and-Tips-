847a34ec19cab72755d4c50ad00a9dfd5b242a52
elsashanas <87591085+elsashanas@users.noreply.github.com>
2021-07-18 10:22:57 +0700

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,9 @@
+#berfungsi apabila ingin bekerja dengan modul
+
+import matematika as mm
+mm.tambah (3,5)
+mm.kurang (3,5) 
+
+#apa itu main 
+#fungsi name adalah untuk  memanggil nama modul baru yang sudah dibbuat di dalam satu package
+
